1b7d4657d5e18aee7feb93cba2f0114d3c5120e6
Arvin <609534261@qq.com>
2016-09-01 17:41:57 +0800

    Initial Commit

---
+++ test1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  test1
+//
+//  Created by uxian on 16/9/1.
+//  Copyright © 2016年 NumberSeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ test1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  test1
+//
+//  Created by uxian on 16/9/1.
+//  Copyright © 2016年 NumberSeed. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
